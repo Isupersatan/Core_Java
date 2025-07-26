f4b0717f0cdb1ea4c46c54e3769e36f35b191441
sayan pal <sayansuper330@gmail.com>
2025-07-26 12:53:40 +0530

    add

---
+++ jsp/dsa/string/ReverseString.java
@@ -0,0 +1,32 @@
+// reverse a string
+package jsp.dsa.string;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a String");
+		String str = sc.nextLine();
+		System.out.println("The reversed string is "+revString(str));
+		sc.close();
+	}
+	
+	static String revString(String s) {
+		char [] a = s.toCharArray();
+		int i=0, j=a.length-1;
+		while(i<j) {
+			char temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+			i++;
+			j--;
+		}
+		String str = new String(a);
+		return str;
+	}
+	
+}
